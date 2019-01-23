f4bea938e3d9dc0a4bb326c3eda07ef25453a183
GuyDarel <GuyDarel@gmail.com>
2019-01-22 21:45:54 -0800

    Sprite collision detection works BUG: regardless of sprite2 location sprite2 is placed adjacent to sprite.

--- Lootly/core/src/com/roguelike/lootly/GameScreen.java
+++ Lootly/core/src/com/roguelike/lootly/GameScreen.java
@@ -5,14 +5,22 @@ import com.badlogic.gdx.Input;
 import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Matrix4;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
+import com.badlogic.gdx.physics.box2d.Contact;
+import com.badlogic.gdx.physics.box2d.ContactImpulse;
+import com.badlogic.gdx.physics.box2d.ContactListener;
+import com.badlogic.gdx.physics.box2d.EdgeShape;
 import com.badlogic.gdx.physics.box2d.Fixture;
 import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.Manifold;
 import com.badlogic.gdx.physics.box2d.PolygonShape;
 import com.badlogic.gdx.physics.box2d.World;
 import com.badlogic.gdx.scenes.scene2d.Stage;
@@ -26,9 +34,14 @@ public class GameScreen implements Screen, InputProcessor {
 	private Stage stage;
 	
     Sprite sprite;//rogue
+    Sprite sprite2;//mage
     World world;
     Body body;
+    Body body2;
+    Body bodyEdgeScreen;
     final float PIXELS_TO_METERS = 100f;//scale of movement speed
+    final float SCALE = 3;
+    final float SCALE2 = .3f;
     
 	ItemDisplayBox itemBox = new ItemDisplayBox(Lootly.itemList.get(1)); //TODO: Remove debugging object
 	ItemDisplayBox itemBoxClone = new ItemDisplayBox(Lootly.itemList.get(1).clone()); //TODO: Remove debugging object	
@@ -42,26 +55,7 @@ public class GameScreen implements Screen, InputProcessor {
 	public void show() {
 		Gdx.input.setInputProcessor(stage);
 		
-		//Rogue sprite introduction
-		//game.batch
-        sprite = CharacterClassManager.getClassSprite(Classes.ROGUE);
-        sprite.setPosition(Gdx.graphics.getWidth() / 2 - sprite.getWidth() / 2,
-                		   Gdx.graphics.getHeight() / 2);
-        sprite.setScale(5);
-        
-        world = new World(new Vector2(), true);//create world w/o gravity
-        BodyDef bodyDef = new BodyDef();//make body in world
-        bodyDef.type = BodyDef.BodyType.DynamicBody;//define body
-        bodyDef.position.set((sprite.getX() + sprite.getWidth() /2) / PIXELS_TO_METERS, 
-        					 (sprite.getY() + sprite.getHeight()/2) / PIXELS_TO_METERS);//set body position to match sprite adjusted position
-        body = world.createBody(bodyDef);//make body in world
-        PolygonShape shape = new PolygonShape();//make poly
-        shape.setAsBox(sprite.getWidth()/2 / PIXELS_TO_METERS, sprite.getHeight()/2 / PIXELS_TO_METERS);//set poly dimension to sprite adjusted size
-        FixtureDef fixtureDef = new FixtureDef();
-        fixtureDef.shape = shape;//define shape of body
-        fixtureDef.density = .1f;//define weight of body
-        Fixture fixture = body.createFixture(fixtureDef);
-        shape.dispose();
+		newWorld();
         
 		//Actor instantiation
 		itemBox.setX(Gdx.graphics.getWidth()/2);
@@ -74,6 +68,7 @@ public class GameScreen implements Screen, InputProcessor {
 		stage.addActor(itemBox);
 		stage.addActor(itemBoxClone);
 		itemBoxClone.getItem().setSprite(new Sprite(new Texture("item/candy_01g.png")));
+		
 	}
 
 	@Override
@@ -81,27 +76,35 @@ public class GameScreen implements Screen, InputProcessor {
 		
 		getMotionInput();//moves rogue sprite according to wasd and arrowkeys
 		world.step(1f/60f, 6, 2);//update world
+		
 		sprite.setPosition( (body.getPosition().x * PIXELS_TO_METERS) - sprite.getWidth()/2 ,
 							(body.getPosition().y * PIXELS_TO_METERS) -sprite.getHeight()/2 );//set sprite position to box postion
-        sprite.setRotation((float)Math.toDegrees(body.getAngle()));//set sprite rotation to box position
+		sprite.setRotation((float)Math.toDegrees(body.getAngle()));//set sprite rotation to box position
         
+        sprite2.setPosition((body2.getPosition().x * PIXELS_TO_METERS) - sprite2.getWidth()/2 ,
+							(body2.getPosition().y * PIXELS_TO_METERS) -sprite2.getHeight()/2 );//set sprite position to box postion
+        sprite2.setRotation((float)Math.toDegrees(body2.getAngle()));//set sprite rotation to box position
         
-		
 		game.batch.setProjectionMatrix(game.camera.combined);
 		
 		Gdx.gl.glClearColor(0, 0, 0.2f, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		
 		stage.act(delta);
-		stage.draw();
+		//stage.draw();
 		
 		game.batch.begin();
 		game.batch.draw(sprite,    sprite.getX(), sprite.getY(),
 				sprite.getOriginX(),sprite.getOriginY(),
 				sprite.getWidth(),   sprite.getHeight(),
 				sprite.getScaleX(),  sprite.getScaleY(),sprite.getRotation());
+		game.batch.draw(sprite2,    sprite2.getX(), sprite2.getY(),
+						sprite2.getOriginX(),sprite2.getOriginY(),
+						sprite2.getWidth(),   sprite2.getHeight(),
+						sprite2.getScaleX(),  sprite2.getScaleY(),sprite2.getRotation());
 		game.batch.end();
-
+		
+		
 	}
 
 	@Override
@@ -182,17 +185,89 @@ public class GameScreen implements Screen, InputProcessor {
 		return false;
 	}
 	
+	public void newWorld() {     
+        world = new World(new Vector2(), true);//create world w/o gravity
+        
+        sprite = CharacterClassManager.getClassSprite(Classes.ROGUE);
+        sprite.setPosition(Gdx.graphics.getWidth() / 2 - sprite.getWidth() / 2,
+                		   Gdx.graphics.getHeight() * .66f);
+        sprite.setScale(SCALE);
+        
+        BodyDef bodyDef = new BodyDef();//make body in world
+        bodyDef.type = BodyDef.BodyType.DynamicBody;//define body
+        bodyDef.position.set((sprite.getX() + sprite.getWidth() /2) / PIXELS_TO_METERS, 
+        					 (sprite.getY() + sprite.getHeight()/2) / PIXELS_TO_METERS);//set body position to match sprite adjusted position
+        body = world.createBody(bodyDef);//make body in world
+        PolygonShape shape = new PolygonShape();//make poly
+        shape.setAsBox(sprite.getWidth()/2  / PIXELS_TO_METERS * SCALE / 2, 
+        			   sprite.getHeight()/2 / PIXELS_TO_METERS * SCALE);//set poly dimension to sprite adjusted size
+        FixtureDef fixtureDef = new FixtureDef();
+        fixtureDef.shape = shape;//define shape of body
+        fixtureDef.density = 1f;//define weight of body
+        body.createFixture(fixtureDef);
+        body.setFixedRotation(true);
+
+        
+        sprite2 = CharacterClassManager.getClassSprite(Classes.MAGE);
+        sprite2.setPosition(Gdx.graphics.getWidth() / 2 - sprite2.getWidth() / 2,
+                		    Gdx.graphics.getHeight() * .33f);
+        sprite2.setScale(SCALE2);
+        
+        BodyDef bodyDef2 = new BodyDef();//make body in world
+        bodyDef2.type = BodyDef.BodyType.DynamicBody;//define body
+        bodyDef2.position.set((sprite2.getX() + sprite2.getWidth() /2) / PIXELS_TO_METERS, 
+        					  (sprite2.getY() + sprite2.getHeight()/2) / PIXELS_TO_METERS);//set body position to match sprite adjusted position
+        body2 = world.createBody(bodyDef);//make body in world
+        shape = new PolygonShape();//make poly
+        shape.setAsBox(sprite2.getWidth()/2  / PIXELS_TO_METERS * SCALE2 / 2,
+ 			   		   sprite2.getHeight()/2 / PIXELS_TO_METERS * SCALE2);//set poly dimension to sprite adjusted size
+        FixtureDef fixtureDef2 = new FixtureDef();
+        fixtureDef2.shape = shape;
+        fixtureDef2.density = 10f;
+        body2.createFixture(fixtureDef2);
+        body2.setFixedRotation(true);
+        
+        shape.dispose();
+        
+        world.setContactListener(new ContactListener() {
+            @Override
+            public void beginContact(Contact contact) {
+                // Check to see if the collision is between the second sprite and the bottom of the screen
+                // If so apply a random amount of upward force to both objects... just because
+                if((contact.getFixtureA().getBody() == body &&
+                    contact.getFixtureB().getBody() == body2)||
+                   (contact.getFixtureA().getBody() == body2 &&
+                    contact.getFixtureB().getBody() == body)) {
+
+                	
+                }
+            }
+
+            @Override
+            public void endContact(Contact contact) {
+            }
+
+            @Override
+            public void preSolve(Contact contact, Manifold oldManifold) {
+            }
+
+            @Override
+            public void postSolve(Contact contact, ContactImpulse impulse) {
+            }
+        });
+	}
+	
 	public void getMotionInput(){
 		float inpDirX = 0.0f;
 		float inpDirY = 0.0f;
-		if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)||Gdx.input.isKeyPressed(Input.Keys.D)) 
-            inpDirX++;
+		if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)||Gdx.input.isKeyPressed(Input.Keys.D))
+            inpDirX+=5f;
 		if(Gdx.input.isKeyPressed(Input.Keys.LEFT)||Gdx.input.isKeyPressed(Input.Keys.A))
-            inpDirX--;
+            inpDirX-=5f;
 		if(Gdx.input.isKeyPressed(Input.Keys.UP)||Gdx.input.isKeyPressed(Input.Keys.W))
-            inpDirY++;
+            inpDirY+=5f;
 		if(Gdx.input.isKeyPressed(Input.Keys.DOWN)||Gdx.input.isKeyPressed(Input.Keys.S))
-            inpDirY--;
+            inpDirY-=5f;
 		body.setLinearVelocity(inpDirX, inpDirY);
 	}
 	
